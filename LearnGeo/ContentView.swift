8b6dabaa5bfa38a3e60e5923707bea107164d11d
Karl Brycz <kbrycz@umich.edu>
2023-07-08 21:06:45 -0500

    Initial Commit

---
+++ LearnGeo/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  LearnGeo
+//
+//  Created by Karl Brycz on 7/8/23.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LearnGeo/LearnGeoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGeoApp.swift
+//  LearnGeo
+//
+//  Created by Karl Brycz on 7/8/23.
+//
+
+import SwiftUI
+
+@main
+struct LearnGeoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
